a5177028e078bda1e4837648db8f76892b14cc81
CarvalhoSI <guilhermecarvalho.tti@gmail.com>
2017-04-06 20:35:23 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,5 +1,5 @@
 <?php
-	$strConn = "host=localhost dbname=login port=5432 user=postgres password='root'";
+	$strConn = "host=localhost dbname=sipedb port=5432 user=sipe password='senac123'";
 	$con = pg_connect($strConn);
 	if(!$con){
 			echo "<p><b>ERRO DE CONEXÃO COM O BANCO DE DADOS<b></p>";
