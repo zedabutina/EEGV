931856c3da102cf5630264ebb31866428d732aba
scr1ptc0der <viniciusldc@gmail.com>
2017-04-06 21:45:11 -0300

    Add files via upload

---
+++ IncluirDisciplina.php
@@ -0,0 +1,21 @@
+<?php 
+
+if(!$con){
+	header("location:#");
+}
+
+$codigo = $_POST['codigodisc'];
+$nome = $_POST['nomedisc'];
+$ch = $_POST['ch'];
+$ementa = $_POST['ementa'];
+$objetivo = $_POST['objetivo'];
+$bibliografia_basica = $_POST['bibliografia_basica'];
+$bibliografia_complementar = $_POST['bibliografia_complementar'];
+
+
+$insertsql = "INSERT INTO disciplina (codigodisc, nomedisc, ch, ementa, objetivo, bibliografia_basica, bibliografia_complementar ) VALUES ('".$codigodisc."', '".$nomedisc."', '".$ch."', '".$ementa."', '".$objetivo."', '".$bibliografia_basica."' '".$bibliografia_complementar."');";
+
+?>
+
+
+// only save ! terminando o HTML.
