d2d599a434cca936b86c2925b1d268c06990dfdf
CarvalhoSI <guilhermecarvalho.tti@gmail.com>
2017-04-22 11:59:45 -0300

    Add files via upload
    Atualizando permissões

--- menu.php
+++ menu.php
@@ -40,29 +40,51 @@
 	<br/><br/>
 <?php include "conexao.php"; ?>
 	<body>
-	
-		<div id="main" class="container-fluid" align="center">
-			
-			<label for="curso" onClick="javascript: mostrarCurso();"><h3 class="page-header" >Cursos</h3></label>
-				<div style="display:none" id="curso" name="curso">
-					<a href="incluirCurso.php"><img src='images/add.png' width='128' height='128'></a>
-					<a href="alterarCurso.php"><img src='images/editar.png' width='128' height='128'></a>
-				</div></br>
-			<label for="disciplina" onClick="javascript: mostrarDisciplina();"><h3 class="page-header">Disciplinas</h3></label>
-				<div style="display:none" id="disciplina" name="disciplina">
-					<a href="incluirCurso.php"><img src='images/add.png' width='128' height='128'></a>
-					<a href="alterarCurso.php"><img src='images/editar.png' width='128' height='128'></a>
-				</div></br>
-			<label for="professor" onClick="javascript: mostrarProfessor();"><h3 class="page-header">Professores</h3></label>
-				<div style="display:none" id="professor" name="professor">
-					<a href="incluirCurso.php"><img src='images/add.png' width='128' height='128'></a>
-					<a href="alterarCurso.php"><img src='images/editar.png' width='128' height='128'></a>
-				</div></br>
-			<label for="usuario" onClick="javascript: mostrarUsuario();"><h3 class="page-header">Usuários</h3></label>
-				<div style="display:none" id="usuario" name="usuario">
-					<a href="incluirCurso.php"><img src='images/add.png' width='128' height='128'></a>
-					<a href="alterarCurso.php"><img src='images/editar.png' width='128' height='128'></a>
-				</div></br>
+		<?php
+			if($_COOKIE['login'] == '' && $_COOKIE['nivel'] == ''){
+				echo "<b>Você deve estar logado</b>";
+				header('Location: login.php');
+			}elseif($_COOKIE['login'] != '' && $_COOKIE['nivel'] != ''){ 
+		?>
+				<h3 class="page-header"> Bem Vindo <?php echo $_COOKIE['apelido']; ?> </h3>
+				<hr>
+				<div id="main" class="container-fluid" align="center">
+					<?php 
+						if($_COOKIE['nivel'] == 'C' || $_COOKIE['nivel'] == 'A'){
+					?>
+						<label for="curso" onClick="javascript: mostrarCurso();"><h3 class="page-header" >Cursos</h3></label>
+						<div style="display:none" id="curso" name="curso">
+							<a href="incluirCurso.php"><img src='images/add.png' width='128' height='128'></a>
+							<a href="alterarCurso.php"><img src='images/editar.png' width='128' height='128'></a>
+						</div></br>
 				
-		</div>
+						<label for="disciplina" onClick="javascript: mostrarDisciplina();"><h3 class="page-header">Disciplinas</h3></label>
+							<div style="display:none" id="disciplina" name="disciplina">
+								<a href="incluirCurso.php"><img src='images/add.png' width='128' height='128'></a>
+								<a href="alterarCurso.php"><img src='images/editar.png' width='128' height='128'></a>
+							</div></br>
+						<label for="professor" onClick="javascript: mostrarProfessor();"><h3 class="page-header">Professores</h3></label>
+							<div style="display:none" id="professor" name="professor">
+								<a href="incluirCurso.php"><img src='images/add.png' width='128' height='128'></a>
+								<a href="alterarCurso.php"><img src='images/editar.png' width='128' height='128'></a>
+							</div></br>
+					<?php 
+						}
+					?>
+					<?php 
+						if($_COOKIE['nivel'] == 'C'){
+					?>
+					<label for="usuario" onClick="javascript: mostrarUsuario();"><h3 class="page-header">Usuários</h3></label>
+						<div style="display:none" id="usuario" name="usuario">
+							<a href="incluirCurso.php"><img src='images/add.png' width='128' height='128'></a>
+							<a href="alterarCurso.php"><img src='images/editar.png' width='128' height='128'></a>
+						</div></br>
+					<?php 
+						}
+					?>
+				
+				</div>
+			<?php
+			}
+			?>
 <?php include "rodape.php";?>
