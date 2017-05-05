77acc9eaccd5d79958409d59e83971a7ea3b4c99
Emanuel Gomes <sarobadebarro@hotmail.com>
2017-05-05 11:45:59 -0300

    Add files via upload

--- conexao.php
+++ conexao.php
@@ -3,7 +3,5 @@
 	$con = pg_connect($strConn);
 	if(!$con){
 			echo "<img src='images/desconectado.png' width='56' height='56'>";
-	}else{
-		echo "<img src='images/conectado.png' width='56' height='56'>";
 	}
 ?>
