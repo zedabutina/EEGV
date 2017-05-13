5c0a59d03dec6bd9d91e2b2056e92d6524b9ae4a
Emanuel Gomes <sarobadebarro@hotmail.com>
2017-05-13 15:16:08 -0300

    Add files via upload

--- rodape.php
+++ rodape.php
@@ -3,12 +3,9 @@
 	</body>
 	<p align="right">
 	<small>
-		<?php 
-			setlocale(LC_ALL, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
-			date_default_timezone_set('America/Sao_Paulo');
+		<?php
 			echo strftime('%A, %d de %B de %Y', strtotime('today'));$date = date(' | H:i:s');echo $date;echo "s";
 		?>
-</small>
 	</small>
 	</p>
 </html>
