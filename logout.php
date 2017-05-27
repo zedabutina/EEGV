ce1d25c4b377cb59fa84c93ec95cdba7a8beaedb
CarvalhoSI <guilhermecarvalho.tti@gmail.com>
2017-05-26 23:04:05 -0300

    index e else

--- logout.php
+++ logout.php
@@ -3,6 +3,8 @@
 	
 	if(isset($_SESSION['login']) && isset($_SESSION['nivel'])){
 	session_destroy();
-	header ('Location: menu.php');
+	header ('Location: index.php');
+	}else{
+		echo "'<script>alert('Você deve estar logado para isso!');window.location.href='index.php'</script>';"; 
 	}
 ?>
